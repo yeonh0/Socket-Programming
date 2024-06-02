39edfdf6f3aac1a3f4a309658659ef0c06fdcdbf
yeonh0 <nanyeonho@gmail.com>
2024-06-03 04:05:06 +0900

    접속중인 클라이언트 정보 관리 connect_list 만들어서 로그인 성공한 클라이언트 id와 소켓 저장하고, 1. 로그인 할 때 중복 방지, 2. 귓속말 할 때 접속중인지 확인, 3. 접속 종료 시 리스트에서 제거

--- FinalProject/server/clnt_connection.c
+++ FinalProject/server/clnt_connection.c
@@ -1,76 +1,5 @@
 // clnt_connection.c
 #include "clnt_connection.h"
-#include <time.h>
-
-int SIGNUP(char* id) {
-	int retval;
-	int buf[2000];				// UserInfo Data (ID, PW set)
-	char* tok_ptr = NULL;
-	char* id_ptr = NULL;
-
-	// ID has Space or Too long - return 1
-	if (strchr(id, ' ') != NULL || strlen(id) > 15)
-		return 1;
-
-	// Open UserInfo File
-	FILE* fp = fopen("info.txt", "r");
-	retval = fread(buf, sizeof(char), sizeof(buf), fp);
-	fclose(fp);
-	buf[retval] = '\0';
-
-	// Enter Tokenizer
-	tok_ptr = strtok(buf, "\n");
-	while (tok_ptr != NULL) {
-		retval = strlen(tok_ptr);
-		id_ptr = strtok(tok_ptr, " ");
-		tok_ptr = strtok(tok_ptr + retval + 1, "\n");
-
-		if (strcmp(id_ptr, id) == 0)
-			// Duplicated ID - return 2
-			return 2;
-	}
-
-	return 0;
-}
-
-int SIGNIN(char* id, char* pw) {
-	int retval;
-	int signinbuf[2000];				// UserInfo Data (ID, PW set)
-	char* tok_ptr = NULL;
-	char* id_ptr = NULL;
-	char* pw_ptr = NULL;
-
-	// ID or PW has Space or Too long - return 1
-	if (strchr(id, ' ') != NULL || strlen(id) > 15 || strchr(pw, ' ') != NULL || strlen(pw) > 15)
-		return 1;
-
-	// Open UserInfo File
-	FILE* file = fopen("info.txt", "r");
-	if (file == NULL) {
-		fprintf(stderr, "Could not open info.txt for reading\n");
-		return 1;
-	}
-
-	char line[256];
-
-	while (fgets(line, sizeof(line), file)) {
-		// Remove newline character if present
-		line[strcspn(line, "\n")] = 0;
-
-		// Split the line into id and pw based on the first space
-		char* id_ptr = strtok(line, " ");
-		char* pw_ptr = strtok(NULL, " ");
-
-		if (id_ptr && pw_ptr) {
-			if ((strcmp(id, id_ptr) == 0) && (strcmp(pw, pw_ptr) == 0)) {
-				return 2;
-			}
-		}
-	}
-
-	fclose(file);
-	return 1;
-}
 
 DWORD WINAPI clnt_connection(LPVOID arg) {
 	// Local Variable
@@ -82,6 +11,10 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 	int retval;
 	
 	FILE* fp;
+	time_t t;
+	char time_str[26];
+	struct tm* tm_info;
+
 	char* id_ptr = NULL;				// Whisper Dest, Msg, idchk
 	char* pw_ptr = NULL;
 	char* msg_ptr = NULL;
@@ -106,7 +39,7 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 			send(clnt_sock, msg, strlen(msg) + 1, 0);
 
 			// Check ID
-			if (recv(clnt_sock, id, sizeof(id), 0) == SOCKET_ERROR || strcmp(id, "quit") == 0)
+			if ((recv(clnt_sock, id, sizeof(id), 0) == SOCKET_ERROR) || strcmp(id, "quit") == 0)
 				break;
 			retval = SIGNUP(id);
 
@@ -127,7 +60,7 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 			// Get PW - Read Message from the Client
 			sprintf(msg, "\033[0;36m[SYSTEM]Sign Up - Enter PW\033[0m");
 			send(clnt_sock, msg, strlen(msg) + 1, 0);
-			if (recv(clnt_sock, pw, sizeof(pw), 0) == SOCKET_ERROR || strcmp(pw, "quit") == 0)
+			if ((recv(clnt_sock, pw, sizeof(pw), 0) == SOCKET_ERROR) || strcmp(pw, "quit") == 0)
 				break;
 
 			// PW has Space or Too long - return 1
@@ -162,12 +95,12 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 			// Get ID, PW - Read Message from the Client
 			sprintf(msg, "\033[0;36m[SYSTEM]Sign In - Enter ID\033[0m");
 			send(clnt_sock, msg, strlen(msg) + 1, 0);
-			if (recv(clnt_sock, id, sizeof(id), 0) == SOCKET_ERROR || strcmp(id, "quit") == 0)
+			if ((recv(clnt_sock, id, sizeof(id), 0) == SOCKET_ERROR) || strcmp(id, "quit") == 0)
 				break;
 
 			sprintf(msg, "\033[0;36m[SYSTEM]Sign In - Enter PW\033[0m");
 			send(clnt_sock, msg, strlen(msg) + 1, 0);
-			if (recv(clnt_sock, pw, sizeof(pw), 0) == SOCKET_ERROR || strcmp(pw, "quit") == 0)
+			if ((recv(clnt_sock, pw, sizeof(pw), 0) == SOCKET_ERROR) || strcmp(pw, "quit") == 0)
 				break;
 
 			// Check ID & PW
@@ -180,8 +113,19 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 				continue;
 			}
 
-			// Incorrect ID PW
+			// Correct ID PW
 			if (retval == 2) {
+				// Check Duplicate Connecting
+				WaitForSingleObject(hMutex, INFINITE);
+				for (int i = 0; i < g_log_count; i++) {
+					if (strcmp(id, g_connect_list[i].con_id) == 0) {
+						sprintf(msg, "\033[0;36m[SYSTEM]Account is already being linked\033[0m");
+						send(clnt_sock, msg, strlen(msg) + 1, 0);
+						continue;
+					}
+				}
+				ReleaseMutex(hMutex);
+
 				sprintf(msg, "\033[0;34m[SYSTEM]Hello %s. Enter Message.\033[0m", id);
 				send(clnt_sock, msg, strlen(msg) + 1, 0);
 				sprintf(msg, "\033[0;36m%s join chat room\033[0m", id);
@@ -195,6 +139,13 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 		else
 			continue;
 
+		// Add Client list
+		WaitForSingleObject(hMutex, INFINITE);
+		strcpy(g_connect_list[g_log_count].con_id, id);
+		g_connect_list[g_log_count].con_sock = clnt_sock;
+		printf("New : %s, total : %d\n", g_connect_list[g_log_count].con_id, g_log_count+1);
+		g_log_count++;
+		ReleaseMutex(hMutex);
 
 		// 2 : message
 		while (1) {
@@ -207,8 +158,24 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 			if (strncmp(msg, "whi", 3) == 0) {
 				tok_ptr = strtok(msg, " ");
 				id_ptr = strtok(NULL, " ");
-				msg_ptr = strtok(NULL, " ");
-				printf("%s, %s\n", id_ptr, msg_ptr);
+				msg_ptr = id_ptr + strlen(id_ptr) + 1;
+
+				// Search destination ID in connect clients list
+				WaitForSingleObject(hMutex, INFINITE);
+				for (int i = 0; i < g_log_count; i++) {
+					if ((strcmp(id_ptr, g_connect_list[i].con_id) == 0) && strcmp(id_ptr, id) != 0) {
+						// Destination Client Log in - Send Msg
+						printf("%s -> %s : %s\n", id, id_ptr, msg_ptr);
+						sprintf(chat, "\033[0;36m[SEND to %s] : %s\033[0m", id_ptr, msg_ptr);
+						send(clnt_sock, chat, strlen(chat) + 1, 0);
+
+						sprintf(chat, "\033[0;36m[FROM %s] : %s\033[0m", id, msg_ptr);
+						send(g_connect_list[i].con_sock, chat, strlen(chat) + 1, 0);
+
+						break;
+					}
+				}
+				ReleaseMutex(hMutex);
 				continue;
 			}
 
@@ -224,10 +191,8 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 			}
 
 			// Write chat
-			time_t t;
 			time(&t);
-			char time_str[26];
-			struct tm* tm_info = localtime(&t);
+			tm_info = localtime(&t);
 			strftime(time_str, 26, "%Y-%m-%d %H:%M:%S", tm_info);
 
 			if (fprintf(fp, "[%s] %s\n", time_str, chat) < 0) {
@@ -242,13 +207,16 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 			send(clnt_sock, chat, strlen(chat) + 1, 0);
 		}
 		// Log Out
-		sprintf(msg, "\033[0;35m[%s] has left chat room\033[0m", id);
-		printf("%s\n", msg);
-		send_all_clnt(msg, clnt_sock);
+		if(id != NULL) {
+			sprintf(msg, "\033[0;35m[%s] has left chat room\033[0m", id);
+			printf("%s\n", msg);
+			send_all_clnt(msg, clnt_sock);
+			break;
+		}
 	}
 
-	// Remove the client socket from the socket list
 	WaitForSingleObject(hMutex, INFINITE);
+	// Remove the client socket from the socket list
 	for (int i = 0; i < g_clnt_count; i++) {
 		if (clnt_sock == g_clnt_socks[i]) {
 			for (; i < g_clnt_count - 1; i++)
@@ -257,6 +225,17 @@ DWORD WINAPI clnt_connection(LPVOID arg) {
 		}
 	}
 	g_clnt_count--;
+
+	// Remove the login client from the losgin client list
+	for (int i = 0; i < g_log_count; i++) {
+		if (strcmp(id, g_connect_list[i].con_id) == 0) {
+			printf("Disappear : %s, total : %d\n", g_connect_list[i].con_id, g_log_count - 1);
+			for (; i < g_log_count - 1; i++)
+				g_connect_list[i] = g_connect_list[i + 1];
+			g_log_count--;
+			break;
+		}
+	}
 	ReleaseMutex(hMutex);
 
 	// Close the Client Socket
--- FinalProject/server/clnt_connection.h
+++ FinalProject/server/clnt_connection.h
@@ -1,6 +1,9 @@
 // clnt_connection.h
 #ifndef CLNT_CONNECTION_H
 #define CLNT_CONNECTION_H
+
+////////////////////////////////////////////////////////////////////////////////
+// Define & Header File
 #define _WINSOCK_DEPRECATED_NO_WARNINGS 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -9,15 +12,25 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <Windows.h>
+#include <time.h>
 #include "ErrorHandling.h"
 
 #define BUFSIZE    1000		// Message Buffer Size
+
 ////////////////////////////////////////////////////////////////////////////////
 // Global Var
-extern SOCKET g_clnt_socks[];  // Socket List
-extern int g_clnt_count;               // Client Count
+extern SOCKET g_clnt_socks[];						// Socket List
+extern int g_clnt_count;							// Client Count
+extern int g_log_count;								// Client Count
 
-extern HANDLE hMutex;                  // Socket List Mutex
+typedef struct connect_list {
+	char con_id[30];							// Connect Clients List
+	SOCKET con_sock;
+};
+
+struct connect_list g_connect_list[300];		// Connect Clients List
+
+extern HANDLE hMutex;								// Socket List Mutex
 
 DWORD WINAPI clnt_connection(LPVOID arg);
 
